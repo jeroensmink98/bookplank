f17e60a855a48dd30321f64b3c9a2c4bfedcdf5c
Jeroen Smink <jeroensmink98@gmail.com>
2024-05-22 18:25:56 +0200

    feat: basic prisma
    Changelog: feature

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,10 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
  
-export const { handle } = SvelteKitAuth({
+const prisma = new PrismaClient()
+ 
+export const { handle, signIn, signOut } = SvelteKitAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [],
 })
